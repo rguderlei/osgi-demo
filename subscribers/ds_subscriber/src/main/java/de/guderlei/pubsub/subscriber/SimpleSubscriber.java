2962866f5dabe1a769837fc323e9dfd272129083
Ralph Guderlei <rguderlei@n149.(none)>
2010-03-15 16:05:32 +0100

    comments added

--- subscribers/ds_subscriber/src/main/java/de/guderlei/pubsub/subscriber/SimpleSubscriber.java
+++ subscribers/ds_subscriber/src/main/java/de/guderlei/pubsub/subscriber/SimpleSubscriber.java
@@ -5,16 +5,28 @@ import org.osgi.service.log.LogService;
 import de.guderlei.pubsub.model.Message;
 import de.guderlei.pubsub.model.Subscriber;
 
+/**
+ * simple {@link Subscriber} implementation. The Service will be managed by DS / SCR
+ * 
+ * @author rguderlei
+ *
+ */
 public class SimpleSubscriber implements Subscriber {
 	
 	private LogService log;
 	
+	/**sets the log service reference
+	 * @param log
+	 */
 	public void setLog(LogService log){
 		if(log != null){
 			this.log = log;
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * */
 	@Override
 	public void receive(Message message) {
 		log.log(LogService.LOG_INFO, "SCR Subscriber: " + message.toString());
